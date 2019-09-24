a566248637149dc5e7adcd2ef40137bb3ccbbeee
Julio <juhume.exe@gmail.com>
2019-09-24 21:14:03 +0200

    update profile done

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,27 @@ router.get("/:username", access.checkLogin, (req, res, next) => {
   res.render("profile/profileID");
 });
 
+router.get("/:username/editProfile", access.checkLogin, (req, res, next) => {
+  res.render("profile/editProfile");
+});
+
+router.post("/:username/editProfile", access.checkLogin, (req, res, next) => {
+  const { name, surname, username, bio, location } = req.body;
+  // const { originalname, url } = req.file;
+
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { name, surname, username, bio, location },
+    { new: true }
+  )
+    .then(updatedUser => {
+      res.redirect(`/profile/${req.user.username}`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 // router.post()
 
 module.exports = router;
