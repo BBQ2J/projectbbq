e04ae848b560b31fc1d1c5ca6f99803c45463e98
Julio <juhume.exe@gmail.com>
2019-09-25 18:55:52 +0200

    users seed

--- routes/profile.js
+++ routes/profile.js
@@ -11,13 +11,13 @@ router.get("/:username/editProfile", access.checkLogin, (req, res, next) => {
   res.render("profile/editProfile");
 });
 
-router.post("/:username/editProfile", access.checkLogin, (req, res, next) => {
+router.post("/:username/editProfile", [access.checkLogin, upload.single("userPhoto")], (req, res, next) => {
   const { name, surname, username, bio, location } = req.body;
-  // const { originalname, url } = req.file;
+  const { url } = req.file || "";
 
   User.findOneAndUpdate(
     { _id: req.user._id },
-    { name, surname, username, bio, location },
+    { name, surname, username, bio, location, photo: url },
     { new: true }
   )
     .then(updatedUser => {
@@ -38,43 +38,3 @@ router.post("/:username/editProfile", access.checkLogin, (req, res, next) => {
 // router.post()
 
 module.exports = router;
-
-
-// const router = require("express").Router();
-// const upload = require("./../config/cloudinary.config");
-// const access = require("./../middlewares/access.mid");
-// const User = require("../models/User");
-// router.get("/:username", access.checkLogin, (req, res, next) => {
-//   res.render("profile/profileID");
-// });
-// ​
-// router.get("/:username/editProfile", access.checkLogin, (req, res, next) => {
-//   res.render("profile/editProfile");
-// });
-// ​
-// router.post("/:username/editProfile", access.checkLogin, (req, res, next) => {
-//   const { name, surname, username, bio, location } = req.body;
-//   // const { originalname, url } = req.file;
-// ​
-//   User.findOneAndUpdate(
-//     { _id: req.user._id },
-//     { name, surname, username, bio, location },
-//     { new: true }
-//   )
-//     .then(updatedUser => {
-//       res.redirect(`/profile/${req.user.username}`);
-//     })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// ​
-//   User.findOne({ username }, "username", (err, user) => {
-//       if (user !== null) {
-//         res.render("profile/editProfile", { message: "The username already exists" });
-//         return;
-//       }
-//     })
-// });
-// ​
-// module.exports = router;
-
