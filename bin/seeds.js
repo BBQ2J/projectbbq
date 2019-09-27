e4a692595bec71eebe20d57f218def2bb8641149
JesusSP <jesus.saints@gmail.com>
2019-09-27 11:40:26 +0200

    trying pull after css

--- bin/seeds.js
+++ bin/seeds.js
@@ -99,16 +99,16 @@ let users = [
   }
 ]
 
-// const events = {
-//     title: String,
-//     content: String,
-//     date: { type: Date, min: Date.now },
-//     location: String,
-//     creatorId: { type : Schema.Types.ObjectId, ref: 'User' },
-//     picName: String,
-//     picPath: String,
-//     comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
-//   },
+const events = {
+    title: "Piscina y Juegos",
+    content: "Hola chicos! Os apetece quedar este domingo en la piscina y llevo juegos o algo?",
+    date: { type: Date, min: Date.now },
+    location: String,
+    creatorId: { type : Schema.Types.ObjectId, ref: 'User' },
+    picName: String,
+    picPath: String,
+    comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
+  },
 //     {
 //       title: String,
 //       content: String,
